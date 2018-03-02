4d8a7fd8573568c1c683539358cc695083137096
Akbar Abustang <akbar_abustang@ymail.com>
2018-03-02 19:30:16 +0700

    bismillah revisi fix

--- application/views/soal/tbl_soal_form.php
+++ application/views/soal/tbl_soal_form.php
@@ -24,22 +24,13 @@
 						 ?>
 				</select>
 	   			 <?php } else{ ?>
-	   			 <select>
-	   			 	<?php 
-	   			 		foreach ($matauji_data as $matauji ) {
-	   			 			echo "<option value='$matauji->id_matauji'>";
-	   			 			echo $soal_data['id_matauji']==$matauji->id_matauji?'selected':'';
-	   			 			echo ">$matauji->nama_matauji</option>";
-	   			 		}
-	   			 	 ?>
-	   			 </select>
-	   			 <?php } ?>
 
 	   			<select name="id_matauji" id="id_matauji" class="form-control">
 	   				<?php foreach ($matauji_data as $matauji): ?>
 	   				<option value="<?php echo $matauji->id_matauji ?>"><?php echo $matauji->nama_matauji ?></option>
 	   				<?php endforeach ?>
 	   			</select>
+	   			 <?php } ?>
 	   		</td>
 
 	   	</tr>
