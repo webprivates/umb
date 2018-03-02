403c79416a84a516fb289a07c17e32f5af739ab5
wiwik ramna <wiwikramna@gmail.com>
2018-03-02 15:30:37 +0700

    mata uji

--- application/views/matauji/tbl_matauji_list.php
+++ application/views/matauji/tbl_matauji_list.php
@@ -5,7 +5,7 @@
                 <div class="box box-warning box-solid">
     
                     <div class="box-header">
-                        <h3 class="box-title">KELOLA DATA TBL_MATAUJI</h3>
+                        <h3 class="box-title">KELOLA DATA MATAUJI</h3>
                     </div>
         
         <div class="box-body">
